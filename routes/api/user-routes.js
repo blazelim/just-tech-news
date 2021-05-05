8a5ef8290749d3dc859f9bc1a322fa5b72327449
Blaze Lim <blazebentleycolim@gmail.com>
2021-05-05 00:56:29 -0700

    added password verification and hashing

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -52,6 +52,35 @@ router.get('/:id', (req,res) => {
     });
 
 
+router.post('/login', (req,res) => {
+    // Query Operation
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }) .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!'});
+            return;
+        }
+
+        // res.json({ user: dbUserData });
+        /// Verify user
+
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect Password'});
+            return;
+        }
+
+        res.json({ user: dbUserData, message: 'You are now logged in!'});
+    })
+
+})
+
+
 // PUT /api/users/1
 // associated SQL syntax
 // UPDATE users
@@ -60,18 +89,20 @@ router.get('/:id', (req,res) => {
 
 router.put('/:id', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
-
-    // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
+  
+    // pass in req.body instead to only update what's passed through
     User.update(req.body, {
-        where: {
-            id: req.params.id
-        }
+      individualHooks: true,
+      where: {
+        id: req.params.id
+      }
     })
         .then(dbUserData => {
             if(!dbUserData[0]) {
                 res.status(404).json({ message: 'No user found with this id'});
-                return
-            }''
+                return;
+            }
+            res.json(dbUserData);
         })
         .catch(err => {
             console.log(err);
@@ -98,5 +129,7 @@ router.delete('/:id', (req,res) => {
             res.status(500).json(err);
         });
 });
+
+
 
 module.exports = router;
