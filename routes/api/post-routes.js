f39f4285068398360cd2c315e9f95e57a5b9d600
Blaze Lim <blazebentleycolim@gmail.com>
2021-05-07 18:35:45 -0700

    added endpoint to include comments

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,15 +1,30 @@
 const router = require('express').Router();
 
-const { Post, User, Vote } =require ('../../models');
+const { Post, User, Vote, Comment } =require ('../../models');
 const { sequelize } = require('../../models/User');
 
 // get all posts
 router.get('/', (req,res) => {
     console.log('===============================');
     Post.findAll({
-        attributes: ['id', 'post_url', 'title', 'created_at'],
         order: [['created_at', 'DESC']],
+        attributes: [
+            'id',
+            'post_url', 
+            'title', 
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
         include: [
+
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
@@ -71,13 +86,12 @@ router.put('/upvote', (req, res) => {
     // custom static method created in models/Post.js
     Post.upvote(req.body, { Vote })
         .then(updatedPostData => res.json(updatedPostData))
-        })
         .catch(err => {
           console.log(err);
           res.status(400).json(err);
         });
     });
-});
+
 
 //update a post title
 router.put('/:id', (req, res) => {
