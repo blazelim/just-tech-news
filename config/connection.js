e505b9597c6fe42a871814f94edef293c26fd897
Blaze Lim <blazebentleycolim@gmail.com>
2021-05-07 18:48:40 -0700

    Typo in last commit

--- config/connection.js
+++ config/connection.js
@@ -6,17 +6,14 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-    // create connection to our database, pass in your MYSQL information for username and password
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
-});
+  });
 }
-
-
 
 
 module.exports = sequelize;
