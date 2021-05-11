9245c9cffcbb500b1a9671ee3841bdb18e733214
Blaze Lim <blazebentleycolim@gmail.com>
2021-05-11 14:19:06 -0700

    fixed typo on PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const routes = require('./routes');
 const sequelize = require('./config/connection.js');
 
 const app = express();
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
