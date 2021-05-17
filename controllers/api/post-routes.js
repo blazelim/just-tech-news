e394862b4aba7a2ffc206f32329f288b6a46431f
Blaze Lim <blazebentleycolim@gmail.com>
2021-05-17 00:09:50 -0700

    added helpers

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -137,18 +137,18 @@ router.delete('/:id', (req, res) => {
     });
 });
 
-router.put('/upvote', (req,res) => {
+router.put('/upvote', (req, res) => {
     // make sure the session exists first
     if (req.session) {
-        // pass session id along with all destructured properties on req.body
-        Post.upvote({ ...req.body, user_id: req.session.user_id }, {Vote, Comment, User})
-            .then(updateVoteData => res.json(updateVoteData))
-            .catch(err => {
-                console.log(err);
-                res.status(500).json(err);
-            });
+      // pass session id along with all destructured properties on req.body
+      Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+        .then(updatedVoteData => res.json(updatedVoteData))
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
     }
-});
+  });
 
 
 
